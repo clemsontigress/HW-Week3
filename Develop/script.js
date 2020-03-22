6c533cff42bdad13c4d29dafa766fa0a22e40f24
clemson tigress <anreeves83@gmail.com>
2020-03-21 23:28:48 -0400

    Homework 3 Final

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +10,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
